7d44d4ef560b278f6f1d31549ff1b6a8b4a9b9ae
Alexander Panis <alpa@sitecore.net>
2020-10-28 12:17:43 +0800

    Add nav buttons in the nav bar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import Link from "next/link";
+
 import Layout from "../components/Layout";
 
-const Index = () => <Layout><h1>Contents here</h1></Layout>;
+const Index = () => (
+    <Layout>
+      <br />
+      <Link href="/explore">
+        <a> Welcome to Gamer's Lobby! Start Exploring Now</a>
+      </Link>
+    </Layout>
+  );
 
 export default Index;
 
