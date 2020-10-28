32e8942a12e9a907580f96af1ad0915a76975192
Alexander Panis <alpa@sitecore.net>
2020-10-28 11:58:20 +0800

    pushing latest UI changes

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+ 
+  import "./Header.scss";
+  import Link from "next/link";
+
+  const Header = props => (
+  <Link href="/">
+    <div className="Header">{props.appTitle}</div>
+  </Link>
+);
+
+  export default Header;
