b4607c4021f765db524d7bafca7003e6b754b2b9
Alexander Panis <alpa@sitecore.net>
2020-10-28 18:56:18 +0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,13 @@ import Layout from "../components/Layout";
 
 const Index = () => (
     <Layout>
-        <table>
-        <tr>
-            <td>
-            <p>
-            <Link href="/explore">
-                <a> Welcome to Gamer's Lobby! Start Exploring Now</a>        
-            </Link>
-            </p>
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <script src="//alpa-xpsingle-single.azurewebsites.net/bundle/beacon"></script>
-            </td>
-        </tr>
-      </table>
-    </Layout>
+        <p>
+        <Link href="/explore">
+            <a> Welcome to Gamer's Lobby! Start Exploring Now</a>        
+        </Link>
+        </p>
+      <script src="//alpa-xpsingle-single.azurewebsites.net/bundle/beacon"></script>
+    </Layout>    
   );
 
 export default Index;
